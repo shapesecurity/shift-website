26d97fdfa49fafba9268337d857af3552060b5e5
Kevin Gibbons <kevin@shapesecurity.com>
2017-04-19 16:13:33 -0700

    Update build script and packages

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 set -x
-cjsify ../shift-codegen-js/dist/index.js -x codegen --no-node --minify > js/shift-codegen.js
-cjsify ../shift-fuzzer-js/dist/index.js -x fuzzer --no-node --minify > js/shift-fuzzer.js
-cjsify ../shift-parser-js/dist/index.js -x parser --no-node --minify > js/shift-parser.js
-cjsify ../shift-reducer-js/dist/index.js -x reducer --no-node --minify > js/shift-reducer.js
-cjsify ../shift-scope-js/dist/index.js -x scope --no-node --minify > js/shift-scope.js
-cjsify ../shift-validator-js/dist/index.js -x validator --no-node --minify > js/shift-validator.js
+./node_modules/.bin/cjsify "`node -e \"console.log(require.resolve('shift-codegen'))\"`" -x codegen --no-node --minify > js/shift-codegen.js
+./node_modules/.bin/cjsify "`node -e \"console.log(require.resolve('shift-fuzzer'))\"`" -x fuzzer --no-node --minify > js/shift-fuzzer.js
+./node_modules/.bin/cjsify "`node -e \"console.log(require.resolve('shift-parser'))\"`" -x parser --no-node --minify > js/shift-parser.js
+./node_modules/.bin/cjsify "`node -e \"console.log(require.resolve('shift-reducer'))\"`" -x reducer --no-node --minify > js/shift-reducer.js
+./node_modules/.bin/cjsify "`node -e \"console.log(require.resolve('shift-scope'))\"`" -x scope --no-node --minify > js/shift-scope.js
+./node_modules/.bin/cjsify "`node -e \"console.log(require.resolve('shift-validator'))\"`" -x validator --no-node --minify > js/shift-validator.js
